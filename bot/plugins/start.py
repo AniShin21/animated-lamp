aa01a78d0659d5e0ea2f2d5e82972e52c49f930a
AniShin <minecraft2727k@gmail.com>
2025-01-12 16:25:46 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,14 +1,11 @@
-from pyrogram import filters
+from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
 from ..screenshotbot import ScreenShotBot
 
-
 @ScreenShotBot.on_message(filters.private & filters.command("start"))
-async def start(c, m):
-
-    await m.reply_text(
-        text=f"Hi there {m.from_user.mention}.\n\nI'm Screenshot Generator Bot. I can provide screenshots from "
+async def start(client, message):
+    await message.reply_text(
+        text=f"Hi there {message.from_user.mention}.\n\nI'm Screenshot Generator Bot. I can provide screenshots from "
         "your video files without downloading the entire file (almost instantly). For more details check /help.",
         quote=True,
         reply_markup=InlineKeyboardMarkup(
