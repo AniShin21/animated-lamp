bb8331be90539255f698446d80383c8b865b77a6
AniShin <minecraft2727k@gmail.com>
2025-01-12 14:01:37 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -4,10 +4,18 @@ from .screenshotbot import ScreenShotBot
 from .config import Config
 
 if __name__ == "__main__":
-
-    logging.basicConfig(level=logging.DEBUG if Config.DEBUG else logging.INFO)
+    # Configure logging
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.DEBUG if Config.DEBUG else logging.INFO,
+    )
     logging.getLogger("pyrogram").setLevel(
-        logging.INFO if Config.DEBUG else logging.WARNING
+        logging.DEBUG if Config.DEBUG else logging.WARNING
     )
 
-    ScreenShotBot().run()
+    # Initialize and run the bot
+    try:
+        bot = ScreenShotBot()
+        bot.run()
+    except Exception as e:
+        logging.error(f"Failed to start the bot: {e}", exc_info=True)
